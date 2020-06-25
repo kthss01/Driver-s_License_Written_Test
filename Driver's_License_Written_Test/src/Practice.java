cd6f2d15bb35a19a3de14ca521394ca2a31f9d89
김태훈 <kthss01@naver.com>
2020-06-26 00:34:50 +0900

    운전면허 시험필기 앱을 위한 데이터 가공 및 한글 테스트

---
+++ Driver's_License_Written_Test/src/Practice.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Practice {
+
+	public static void main(String[] args) throws IOException {
+
+		// 한글 비교 Test
+//		System.out.println("한글".equals("한글"));
+//		System.out.println("영어".equals("한글"));
+//		System.out.println("한글영어".contains("한글"));
+		
+		// 한글 문서 입력 Test
+		BufferedReader br = new BufferedReader(new FileReader("problems.txt"));
+		while (true) {
+			String line = br.readLine();
+			if (line == null) break;
+			System.out.println(line);
+		}
+		br.close();
+	}
+
+}
