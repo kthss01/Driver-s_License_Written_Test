a672c389f57d0645bfc6c9579b2194e25bd21c48
김태훈 <kthss01@naver.com>
2020-06-27 03:09:54 +0900

    windowbuilder 이용하여 gui 구현 (테스트)

---
+++ Driver's_License_Written_Test/src/WindowBuilder.java
@@ -0,0 +1,116 @@
+import java.awt.EventQueue;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JFrame;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.io.IOException;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JRadioButton;
+import javax.swing.JLabel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+
+public class WindowBuilder {
+
+	private static ProblemBank bank;
+	
+	private JFrame frame;
+	static JTextArea textArea;
+	
+	/**
+	 * Launch the application.
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		bank = new ProblemBank();
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WindowBuilder window = new WindowBuilder();
+					window.frame.setVisible(true);
+					
+					Problem p = bank.getProblem(0);
+					textArea.setText(p.problem);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public WindowBuilder() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+
+		frame = new JFrame();
+		frame.setFont(new Font("Dialog", Font.PLAIN, 12));
+		frame.setTitle("\uC6B4\uC804\uBA74\uD5C8\uC2DC\uD5D8 \uD544\uAE30\uC571");
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 1280, 720);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(45, 10, 1274, 691);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		textArea = new JTextArea();
+		textArea.setEditable(false);
+		textArea.setLineWrap(true);
+		textArea.setFont(new Font("Monospaced", Font.PLAIN, 74));
+		textArea.setBounds(109, 87, 765, 219);
+		//panel.add(textArea);
+		
+		JTextArea textArea_1 = new JTextArea();
+		textArea_1.setBounds(947, 22, 246, 636);
+		panel.add(textArea_1);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
+		rdbtnNewRadioButton.setBounds(153, 376, 121, 23);
+		panel.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("New radio button");
+		rdbtnNewRadioButton_1.setBounds(330, 376, 121, 23);
+		panel.add(rdbtnNewRadioButton_1);
+		
+		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("New radio button");
+		rdbtnNewRadioButton_2.setBounds(497, 376, 121, 23);
+		panel.add(rdbtnNewRadioButton_2);
+		
+		JRadioButton rdbtnNewRadioButton_3 = new JRadioButton("New radio button");
+		rdbtnNewRadioButton_3.setBounds(673, 376, 121, 23);
+		panel.add(rdbtnNewRadioButton_3);
+		
+		ButtonGroup group = new ButtonGroup();
+		group.add(rdbtnNewRadioButton);
+		group.add(rdbtnNewRadioButton_1);
+		group.add(rdbtnNewRadioButton_2);
+		group.add(rdbtnNewRadioButton_3);
+		
+		JTextArea textArea_2 = new JTextArea();
+		textArea_2.setBounds(120, 441, 757, 94);
+		panel.add(textArea_2);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(92, 57, 664, 274);
+		scrollPane.setViewportView(textArea);
+		panel.add(scrollPane);
+		
+
+	}
+}
